aa40851d534ddade427d399449309a51e6d482f4
SeanLiu <seanliu@bluetechnology.com.tw>
2024-03-21 16:11:12 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 練習Swagger 3 in Spring 3
 
 提供GET與POST傳遞參數的範例、前端物件的設計
+
+http://localhost:8080/swaggerDemo/swagger-ui/index.html
--- src/main/java/com/example/swaggerdemo/controller/DemoController.java
+++ src/main/java/com/example/swaggerdemo/controller/DemoController.java
@@ -1,14 +1,7 @@
 package com.example.swaggerdemo.controller;
 
-import com.example.swaggerdemo.vo.EditVO;
-import io.swagger.v3.oas.annotations.ExternalDocumentation;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.extern.slf4j.Slf4j;
+import java.time.LocalDateTime;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,11 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.time.LocalDateTime;
+import com.example.swaggerdemo.vo.EditVO;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
@@ -28,28 +25,17 @@ import java.time.LocalDateTime;
 @Tag(name = "FUNCTION01")
 public class DemoController {
 
-    @Operation(summary = "這是一個GET請求範例",
-            description = "測試GET ...",
-            responses = {@ApiResponse(responseCode = "200", description = "成功"),
-                    @ApiResponse(responseCode = "404", description = "找不到路徑")},
-            parameters = {@Parameter(name = "oid", description = "參數1")},
-            externalDocs = @ExternalDocumentation(url = "https://xxx/README.md")
-    )
-    @GetMapping(value = "testget")
-    public String get(@RequestParam String oid) {
-        return LocalDateTime.now() + " oid: " + oid;
-    }
+	@Operation(summary = "這是一個GET請求範例", description = "測試GET ...", responses = { @ApiResponse(responseCode = "200", description = "成功"), @ApiResponse(responseCode = "404", description = "找不到路徑") }, parameters = { @Parameter(name = "oid", description = "參數1") }, externalDocs = @ExternalDocumentation(url = "https://xxx/README.md"))
+	@GetMapping(value = "testget")
+	public String get(@RequestParam String oid) {
+		return LocalDateTime.now() + " oid: " + oid;
+	}
 
-    @Operation(summary = "這是一個Post請求範例",
-            description = "測試Post ...",
-            responses = {@ApiResponse(responseCode = "200", description = "成功",content = @Content(mediaType = "application/json")),
-                    @ApiResponse(responseCode = "404", description = "找不到路徑")},
-            externalDocs = @ExternalDocumentation(url = "https://xxxooo/README.md")
-    )
-    @PostMapping(value = "testpost")
-    public String post(@RequestBody EditVO vo) {
+	@Operation(summary = "這是一個Post請求範例", description = "測試Post ...", responses = { @ApiResponse(responseCode = "200", description = "成功", content = @Content(mediaType = "application/json")), @ApiResponse(responseCode = "404", description = "找不到路徑") }, externalDocs = @ExternalDocumentation(url = "https://xxxooo/README.md"))
+	@PostMapping(value = "testpost")
+	public String post(@RequestBody EditVO vo) {
 
-        return LocalDateTime.now() + " oid: " + vo.getOid();
-    }
+		return LocalDateTime.now() + " oid: " + vo.getOid();
+	}
 
 }
